0f317d35db32ce5a0cf54df52f457ea76c3d0444
Shigma <1700011071@pku.edu.cn>
2022-05-03 19:33:11 +0800

    docs: add manifest

---
+++ docs/index.js
@@ -0,0 +1,13 @@
+module.exports = {
+  text: '教学系统 (Teach)',
+  children: [
+    '/community/dialogue/',
+    '/community/dialogue/interp.md',
+    '/community/dialogue/prob.md',
+    '/community/dialogue/regexp.md',
+    '/community/dialogue/context.md',
+    // '/community/dialogue/flow.md',
+    '/community/dialogue/misc.md',
+    '/community/dialogue/config.md',
+  ],
+}
