dbe0b31949c8a2845b59042d4ce755ac15379e87
Shigma <shigma10826@gmail.com>
2023-09-04 02:07:20 +0800

    docs: setup i18n docs

---
+++ docs/.vitepress/config/index.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from '@koishijs/vitepress'
+
+const isDev = process.env.NODE_ENV === 'development' || process.env.VERCEL_ENV === 'preview'
+
+export default defineConfig({
+  title: 'koishi-plugin-dialogue',
+
+  head: [
+    ['link', { rel: 'icon', href: 'https://koishi.chat/logo.png' }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#5546a3' }],
+  ],
+
+  locales: {
+    'zh-CN': require('./zh-CN'),
+    ...(isDev ? {
+    } : {}),
+  },
+
+  themeConfig: {
+    indexName: 'common',
+    logo: 'https://koishi.chat/logo.png',
+
+    socialLinks: {
+      github: 'https://github.com/koishijs/koishi-plugin-dialogue',
+    },
+  },
+})
